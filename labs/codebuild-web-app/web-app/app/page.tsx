3cd5305bfbe827805f6427503f7d52c75aad14e7
NghiaPham <phamtrongnghia1105@gmail.com>
2024-07-31 10:09:53 +0700

    add codebuild-web-app lab

---
+++ labs/codebuild-web-app/web-app/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <Image src="/codebuild.png" alt="Logo" width={200} height={200} />
+      <p
+        className="
+          text-center
+          text-2xl
+          text-gray-600
+          font-semibold
+          p-4
+        "
+      >
+        Hello! This is a simple NextJS web app for the {" "}
+        <Link
+          className="text-blue-500 hover:underline"
+          href="https://github.com/phamtrongngh/aws-labs-for-kids/blob/master/labs/codebuild-web-app/README.md"
+        >
+          {"CodeBuild lab."}
+        </Link>
+      </p>
+    </div>
+  );
+}
